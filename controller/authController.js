ae7bf211ae9570601651909c9d32fc055c30ce18
Umar Raza <umar@webevis.com>
2024-10-31 11:34:21 +0500

    updates

--- controller/authController.js
+++ controller/authController.js
@@ -1,16 +1,22 @@
 const account = require("../schema/userSchema");
 
 const Login = async (req, res) => {
-  if (req.body.email && req.body.password) {
-    let user = await account.findOne(req.body);
-    if (user) {
-      res.send(user);
+    if (req.body.email && req.body.password) {
+        let user = await account.findOne(req.body);
+        let data = {
+            email: user?.email,
+            isAdmin: user?.isAdmin,
+            name: user?.name,
+            _id: user?._id,
+        };
+        if (user) {
+            res.send(data);
+        } else {
+            res.send("Invalid Email or Password");
+        }
     } else {
-      res.send("Invalid Email or Password");
+        res.send("Invalid Email or Password");
     }
-  } else {
-    res.send("Invalid Email or Password");
-  }
 };
 
-module.exports = { Login };
+module.exports = {Login};

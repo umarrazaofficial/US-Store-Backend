41dd09d2582e35fe8554d2fe0a72be028aebee4c
Umar Raza <umar@webevis.com>
2024-10-31 14:45:34 +0500

    updates

--- schema/expensesSchema.js
+++ schema/expensesSchema.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
-const expensesSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+const expensesSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        paidTo: {
+            type: String,
+            required: false,
+        },
+        amount: {
+            type: String,
+            required: true,
+        },
+        propertyId: {
+            type: String,
+            required: true,
+        },
     },
-    paidTo: {
-        type: String,
-        required: false,
-    },
-    amount: {
-        type: String,
-        required: true,
-    },
-    propertyId: {
-        type: String,
-        required: true,
-    },
-});
+    {
+        timestamps: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+);
 module.exports = mongoose.model("expenses", expensesSchema);

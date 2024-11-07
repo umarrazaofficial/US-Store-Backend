a941db5d9b6ad90200bfcb3654461e93c287ad84
Umar Raza <umar@webevis.com>
2024-11-07 11:34:53 +0500

    updated cors

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ require("./config");
 const cors = require("cors");
 const Router = require("./routes/appRouter");
 
+const allowedOrigins = ["https://usstore.vercel.app", "https://us-weather-application.vercel.app/"];
+
+const corsOptions = (req, callback) => {
+    const origin = req.header("Origin");
+    if (allowedOrigins.includes(origin)) {
+        callback(null, {origin: true});
+    } else {
+        callback(null, {origin: false});
+    }
+};
+
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", Router);
 
